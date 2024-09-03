a876075a4e6ae76c75338a35e3fd28d800bc9988
JosephYAA <77916190+JosephYAA@users.noreply.github.com>
2024-09-03 14:19:19 +0300

    added course properties

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
 	import { Slider } from '$lib/components/ui/slider';
 	import { Label } from '$lib/components/ui/label';
-	export let noCourses = [3];
+	import { Input } from '$lib/components/ui/input/index.js';
+
+	export let noCourses = [0];
+
+	interface Course {
+		id: number;
+		name: string;
+		credits: number;
+		grade: string;
+	}
+
+	export let courses = new Array<Course>();
+
+	function changeNoCourses(value: number) {
+		noCourses[0] = value;
+		for (let i = courses.length; i < noCourses[0]; i++) {
+			courses[i] = {
+				id: i,
+				name: 'Course ' + (i + 1),
+				credits: 10,
+				grade: 'A-'
+			};
+		}
+		courses.length = noCourses[0];
+	}
 </script>
 
 <div class="mt-20 flex flex-col items-center justify-center space-y-2">
@@ -9,13 +33,16 @@
 	<Slider
 		id="courses"
 		value={noCourses}
-		onValueChange={(v) => (noCourses = v)}
+		onValueChange={(v) => changeNoCourses(v[0])}
 		max={10}
 		step={1}
 		class="max-w-[40%]"
 	/>
 </div>
 <!-- eslint-disable-next-line-->
-{#each { length: noCourses[0] } as _, i}
-	<li>Course {i}</li>
-{/each}
+<form>
+	{#each courses as course}
+		<Label for="course-name">Course Name</Label>
+		<Input type="text" id="course-name" bind:value={course.name} />
+	{/each}
+</form>
