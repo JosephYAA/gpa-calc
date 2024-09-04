000a18a7c47d998fde452da6a80036ba91a4828a
JosephYAA <77916190+JosephYAA@users.noreply.github.com>
2024-09-04 11:14:16 +0300

    added gpa calculation and display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 	import { Slider } from '$lib/components/ui/slider';
 	import { Label } from '$lib/components/ui/label';
 	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from '$lib/components/ui/button';
+	import * as Alert from '$lib/components/ui/alert';
+	import * as Select from '$lib/components/ui/select/index.js';
 
 	export let noCourses = [0];
 
@@ -9,8 +12,22 @@
 		id: number;
 		name: string;
 		credits: number;
-		grade: string;
+		grade: { label: string; value: number };
 	}
+
+	let grades = [
+		{ label: 'A', value: 4.0 },
+		{ label: 'A-', value: 3.75 },
+		{ label: 'B+', value: 3.5 },
+		{ label: 'B', value: 3.0 },
+		{ label: 'B-', value: 2.75 },
+		{ label: 'C+', value: 2.5 },
+		{ label: 'C', value: 2.0 },
+		{ label: 'C-', value: 1.75 },
+		{ label: 'D+', value: 1.3 },
+		{ label: 'D', value: 1.0 },
+		{ label: 'F', value: 0.0 }
+	];
 
 	export let courses = new Array<Course>();
 
@@ -20,15 +37,29 @@
 			courses[i] = {
 				id: i,
 				name: 'Course ' + (i + 1),
-				credits: 10,
-				grade: 'A-'
+				credits: 3,
+				grade: { label: 'A', value: 4.0 }
 			};
 		}
 		courses.length = noCourses[0];
 	}
+
+	let GPA = 0;
+	let visible = false;
+
+	function calculateGPA() {
+		let noCredits: number = 0;
+		let gradePoints: number = 0;
+		for (let i = 0; i < courses.length; i++) {
+			noCredits += Number(courses[i].credits);
+			gradePoints += courses[i].grade.value * Number(courses[i].credits);
+		}
+		GPA = gradePoints / noCredits;
+		visible = true;
+	}
 </script>
 
-<div class="mt-20 flex flex-col items-center justify-center space-y-2">
+<div class="mb-10 mt-20 flex flex-col items-center justify-center space-y-2">
 	<Label for="courses" class="">Number of Courses: {noCourses}</Label>
 	<Slider
 		id="courses"
@@ -39,14 +70,54 @@
 		class="max-w-[40%]"
 	/>
 </div>
-<!-- eslint-disable-next-line-->
+{#if visible}
+	<Alert.Root class="m-auto mb-5 max-w-[20%]">
+		<Alert.Title>Congratulations!</Alert.Title>
+		<Alert.Description>Your GPA is: {GPA}</Alert.Description>
+	</Alert.Root>
+{/if}
 <form>
 	{#each courses as course}
-		<Label for="course-{course.id}-name">Course Name</Label>
-		<Input type="text" id="course-{course.id}-name" bind:value={course.name} />
-		<Label for="course-{course.id}-credits">Course Credits</Label>
-		<Input type="number" id="course-{course.id}-credits" bind:value={course.credits} />
-		<Label for="course-{course.id}-grade">Course Grade</Label>
-		<Input type="text" id="course-{course.id}-grade" bind:value={course.grade} />
+		<div class="mb-2 flex items-center justify-center space-x-3 p-2">
+			<Label for="course-{course.id}-name">Course Name</Label>
+			<Input
+				type="text"
+				class="max-w-[20%]"
+				id="course-{course.id}-name"
+				bind:value={course.name}
+				on:change={() => (visible = false)}
+			/>
+			<Label for="course-{course.id}-credits">Course Credits</Label>
+			<Input
+				type="number"
+				class="max-w-20"
+				id="course-{course.id}-credits"
+				min="1"
+				max="4"
+				on:change={() => (visible = false)}
+				bind:value={course.credits}
+			/>
+			<Select.Root
+				items={grades}
+				onSelectedChange={() => (visible = false)}
+				bind:selected={course.grade}
+			>
+				<Select.Trigger class="max-w-28">
+					<Select.Value placeholder="Select your grade" />
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Group>
+						<Select.Label>Grades</Select.Label>
+						{#each grades as grade}
+							<Select.Item value={grade.value} label={grade.label}>{grade.label}</Select.Item>
+						{/each}
+					</Select.Group>
+				</Select.Content>
+				<Select.Input name="course-{course.id}-grade" />
+			</Select.Root>
+		</div>
 	{/each}
+	<div class="flex justify-center">
+		<Button on:click={calculateGPA}>Calculate GPA</Button>
+	</div>
 </form>
